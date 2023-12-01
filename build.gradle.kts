f37849f998b0050dc7a8836852c2321e288ab46f
IABED <imane.abed@ynov.com>
2023-12-01 14:26:35 +0100

    add jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,30 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.6"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.8.22"
+	java
+	id("org.springframework.boot") version "3.1.3"
+	id("io.spring.dependency-management") version "1.1.3"
+	id("jacoco")
+	kotlin("jvm") version "1.9.0"
 	kotlin("plugin.spring") version "1.8.22"
+	id("info.solidsoft.pitest") version "1.15.0"
 }
 
-group = "com.example"
+group = "com.jicay"
 version = "0.0.1-SNAPSHOT"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+jacoco {
+	toolVersion = "0.8.7"
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
 }
 
 repositories {
@@ -19,12 +32,18 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("com.willowtreeapps.assertk:assertk:0.27.0")
-	testImplementation("net.jqwik:jqwik:1.8.1")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.liquibase:liquibase-core")
+	implementation("org.postgresql:postgresql")
+	implementation("org.springdoc:springdoc-openapi-starter-common:2.1.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+
+	testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 	testImplementation("io.mockk:mockk:1.13.8")
+	testImplementation("com.willowtreeapps.assertk:assertk:0.27.0")
+	testImplementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.15.0")
+	testImplementation("net.jqwik:jqwik-kotlin:1.8.1")
 }
 
 tasks.withType<KotlinCompile> {
@@ -38,6 +57,24 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
-tasks.bootBuildImage {
-	builder.set("paketobuildpacks/builder-jammy-base:latest")
+tasks.register<JacocoReport>("jacocoFullReport") {
+	executionData(tasks.named("test").get())
+	sourceSets(sourceSets["main"])
+
+	reports {
+		xml.required.set(true)
+		html.required.set(true)
+	}
+}
+
+pitest {
+	targetClasses.add("com.jicay.bookmanagement.*")
+	junit5PluginVersion = "1.0.0"
+	avoidCallsTo.set(setOf("kotlin.jvm.internal"))
+	mutators.set(setOf("STRONGER"))
+	threads.set(Runtime.getRuntime().availableProcessors())
+	testSourceSets.addAll(sourceSets["test"])
+	mainSourceSets.addAll(sourceSets["main"])
+	outputFormats.addAll("XML", "HTML")
+	excludedClasses.add("**BookManagementApplication")
 }
